b6b184ab81f7caf18d20bff7d67877f6b3f21764
AndreaOpazo <andree.2593@gmail.com>
2021-09-20 00:29:37 -0300

    realizo feature navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ItemListContainer } from './containers/ItemListContainer';
 import { HomeComponent } from './components/HomeComponent';
 import { ItemDetailContainer } from './containers/ItemDetailContainer';
 import { Footer } from './components/Footer';
+import NavBar from './components/NavBar/NavBar';
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
-        {/* <NavBar/> */}
+      <NavBar />
       <Switch>
         <Route exact path="/" component={HomeComponent} />
         <Route path="/categoria" component={ItemListContainer} />
